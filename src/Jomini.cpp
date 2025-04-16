37d97e7165be4f3e054c558707748b4e1e171c53
Xorrad <monsieurs.aymeric@gmail.com>
2025-04-16 02:58:20 +0200

    feat: add conversions for arrays

--- src/Jomini.cpp
+++ src/Jomini.cpp
@@ -18,6 +18,17 @@ Object::Object(std::vector<std::shared_ptr<Object>> array)
 
 Object::~Object() {}
 
+template <typename T> T Object::As() const {
+    if (m_Type != Type::SCALAR)
+        throw std::runtime_error("Invalid conversion of object to " + std::string(typeid(T).name()));
+    try {
+        return (T) m_Scalar;
+    }
+    catch (std::exception& e) {
+        throw std::runtime_error(std::string(e.what()) + " Invalid conversion of object to " + std::string(typeid(T).name()));
+    }
+}
+
 template <> std::string Object::As() const {
     if (m_Type != Type::SCALAR)
         throw std::runtime_error("Invalid conversion of object to std::string.");
@@ -67,19 +78,30 @@ template <> Date Object::As() const {
     }
 }
 
-template <typename T> T Object::As() const {
-    if (m_Type != Type::SCALAR)
-        throw std::runtime_error("Invalid conversion of object to " + std::string(typeid(T).name()));
-    try {
-        return (T) m_Scalar;
-    }
-    catch (std::exception& e) {
-        throw std::runtime_error(std::string(e.what()) + " Invalid conversion of object to " + std::string(typeid(T).name()));
-    }
-}
-
 template <typename T> std::vector<T> Object::AsArray() const {
     if (m_Type != Type::ARRAY)
         throw std::runtime_error("Invalid conversion of object to array of " + std::string(typeid(T).name()));
-    return std::vector<T>{};
+    std::vector<T> arr;
+    arr.reserve(m_Array.size());
+    try {
+        for (auto obj : m_Array)
+            arr.push_back(obj->As<T>());
+    }
+    catch (std::exception& e) {
+        throw std::runtime_error(std::string(e.what()) + " Invalid conversion of object to array of " + std::string(typeid(T).name()));
+    }
+    return arr;
+}
+
+template std::vector<std::string> Object::AsArray() const;
+template std::vector<int> Object::AsArray() const;
+template std::vector<double> Object::AsArray() const;
+template std::vector<bool> Object::AsArray() const;
+template std::vector<Date> Object::AsArray() const;
+
+template <> std::vector<std::shared_ptr<Object>> Object::AsArray() const {
+    if (m_Type != Type::ARRAY)
+        throw std::runtime_error("Invalid conversion of object to array of object");
+    // TODO: make a deep copy.
+    return m_Array;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+
 #include "Jomini.hpp"
 
 using namespace Jomini;
@@ -6,9 +8,20 @@ using namespace Jomini;
 int main() {
     std::cout << "hello world" << std::endl;
 
+    // Scalar
     std::shared_ptr<Object> o = std::make_shared<Object>("test");
-
     std::cout << o->As<std::string>() << std::endl;
+
+    // Array
+    std::vector<std::shared_ptr<Object>> arr1;
+    for (int i = 0; i < 10; i++)
+        arr1.push_back(std::make_shared<Object>(std::to_string(i)));
+
+    std::shared_ptr<Object> o1 = std::make_shared<Object>(arr1);
+    
+    for (auto v : o1->AsArray<double>())
+        std::cout << v << " ";
+    std::cout << std::endl;
 
     return 0;
 }

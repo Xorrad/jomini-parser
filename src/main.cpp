0944e5aff5f765c36dbfdd13548b215e1fe47444
Xorrad <monsieurs.aymeric@gmail.com>
2025-04-18 19:31:05 +0200

    feat: add tests for operators

--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,27 @@ int main(int argc, char** argv) {
 }
 
 TEST_CASE("[general] basic key-scalar") {
-    std::string content = "key = value";
-    std::shared_ptr<Object> object = ParseString(content);
+    std::shared_ptr<Object> object = ParseFile("tests/general_basic.txt");
 
     CHECK(object->GetType() == Type::OBJECT);
     CHECK(object->GetEntries().size() == 1);
     CHECK(object->Contains("key"));
     CHECK(object->Get("key") != nullptr);
     CHECK(object->Get("key")->GetType() == Type::SCALAR);
-    CHECK(object->Get("key")->As<std::string>() == "valuea");
+    CHECK(object->Get("key")->As<std::string>() == "value");
     CHECK(object->GetOperator("key") == Operator::EQUAL);
+}
+
+TEST_CASE("[general] operators") {
+    std::shared_ptr<Object> object = ParseFile("tests/general_operators.txt");
+
+    CHECK(object->GetType() == Type::OBJECT);
+    CHECK(object->GetEntries().size() == 7);
+    CHECK(object->GetOperator("equal") == Operator::EQUAL);
+    CHECK(object->GetOperator("less") == Operator::LESS);
+    CHECK(object->GetOperator("less_equal") == Operator::LESS_EQUAL);
+    CHECK(object->GetOperator("greater") == Operator::GREATER);
+    CHECK(object->GetOperator("greater_equal") == Operator::GREATER_EQUAL);
+    CHECK(object->GetOperator("not_equal") == Operator::NOT_EQUAL);
+    CHECK(object->GetOperator("not_null") == Operator::NOT_NULL);
 }
